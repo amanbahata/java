6dfd012076336e9d912681b7a039dacdbc47628c
Aman Bahata <bahataaman@gmail.com>
2017-06-25 20:39:56 +0100

    Adds instance variables and constructor

--- Stack.java
+++ Stack.java
@@ -1,4 +1,20 @@
 /**
-*	This program implements a stack for intigers
-*
+*	This program implements a stack for intigers that is able to gold ten values
+*	@author Aman Bahata
 */
+
+public class Stack{
+
+	private int[] stack;
+	private int topOfStack;
+
+
+// Initialize the stack
+	public Stack(){
+		stack = new int[10];  
+		topOfStack = -1;
+	}
+
+
+
+}
