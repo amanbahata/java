dcd2a88d531fc33311e02b3604101178531f9cd8
Aman Bahata <bahataaman@gmail.com>
2017-06-25 20:46:52 +0100

    Adds push() method.

--- Stack.java
+++ Stack.java
@@ -5,16 +5,28 @@
 
 public class Stack{
 
-	private int[] stack;
+	private int[] stck;
 	private int topOfStack;
 
 
 // Initialize the stack
 	public Stack(){
-		stack = new int[10];  
+		stck = new int[10];  
 		topOfStack = -1;
 	}
 
+// Push an item into the stack
 
+	public String push(int number){
+		int size = stck.length;
+
+		if (topOfStack == (size - 1)) {
+			return "Stack is full !!";
+		}
+
+		stck[++topOfStack] = number;
+
+		return  number + " added to the stack.";
+	}
 
 }
