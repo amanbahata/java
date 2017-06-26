787d7cc0f45d1b05bd9b5c9818506b7135acc84c
Aman Bahata <bahataaman@gmail.com>
2017-06-26 18:16:55 +0100

    Adds various constructors to be used depending on the box initialization imputs

--- box/Box.java
+++ box/Box.java
@@ -9,15 +9,30 @@ public class Box{
 	private double height;
 	private double length;
 
-	// Define constructors
-
+	
+	// Construct a clone of a box object
 	public Box(Box box){
 		width = box.getWidth();
 		height = box.getHeight();
 		length = box.getLength();
 	}
 
-	// Define accessor and mutators
+	// Constructor used when all dimensions specified
+	public Box(double width, double height, double length){
+		this.width = width;
+		this.height = height;
+		this.length = length;
+	}
+
+	// Constructor used when a cube is defined
+
+	public Box(double length){
+		width = length;
+		height = length;
+		length = length;
+	}
+
+	// Define accessor and mutator methods for the fields
 
 	public double getWidth(){
 		return width;
