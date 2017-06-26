bf52e7e737dac0d52955507959ade61849956b60
Aman Bahata <bahataaman@gmail.com>
2017-06-26 18:10:48 +0100

    Adds constructor, accessor and mutator methods

--- box/Box.java
+++ box/Box.java
@@ -4,4 +4,28 @@
 */
 
 public class Box{
+
+	private double width;
+	private double height;
+	private double length;
+
+	// Define constructors
+
+	public Box(Box box){
+		width = box.getWidth();
+		height = box.getHeight();
+		length = box.getLength();
+	}
+
+	// Define accessor and mutators
+
+	public double getWidth(){
+		return width;
+	}
+	public double getHeight(){
+		return height;
+	}
+	public double getLength(){
+		return length;
+	}
 }
