91415d80a3cc55837a8087301dcbdb89e2f44d28
Aman Bahata <bahataaman@gmail.com>
2017-06-26 18:40:01 +0100

    Adds constructors to BoxWeight class

--- box/BoxWeight.java
+++ box/BoxWeight.java
@@ -1,11 +1,34 @@
 /**
 *	Class BoxWeight adds weight to the box
-*
+*	@author Aman Bahata
 */
 
 public class BoxWeight extends Box{
-	
+
 	private double weight;
 
-	
+	public BoxWeight(BoxWeight box){
+		super(box);
+		weight = box.getWeight();
+	}
+
+	public BoxWeight(double width, double height, double length, double weigth){
+		super(double width, double height, double length);
+		this.weigth = weight;
+	}
+
+	public BoxWeight(double length, double weight){
+		super(length);
+		this.weight = weigth;
+	}
+
+	public double getWeight(){
+		return weight;
+	}
+
+	public void setWeight(double weigth){
+		this.weigth = weigth;
+		System.out.println("Box weigth changed to : " + weigth);
+	}
+
 }
