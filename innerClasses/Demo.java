037e7078068125c7c8186655917c09b79f5ed7cb
Aman Bahata <bahataaman@gmail.com>
2017-07-17 19:22:21 +0100

    Adds comments

--- innerClasses/Demo.java
+++ innerClasses/Demo.java
@@ -5,8 +5,11 @@
 class Demo{
 
 	public static void main(String[] args){
+
+		// Instantiates object of type Outer_Demo
 		Outer_Demo outerClass = new Outer_Demo();
 
+		
 		outerClass.displayInner();
 	}
 }
--- innerClasses/Outer_Demo.java
+++ innerClasses/Outer_Demo.java
@@ -5,7 +5,8 @@
 
 public class Outer_Demo{
 
-	private class Inner_Demo{
+
+	private class Inner_Demo{     // declared private to restric access from outside the outer class
 		public void print(){
 			System.out.println("Hello from the inner class !!");
 		}
