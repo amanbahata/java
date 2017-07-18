1e9f243f7b016a6eadd228df2364b1f63bc6390d
Aman Bahata <bahataaman@gmail.com>
2017-07-18 22:29:14 +0100

    Creates thread object in main class

--- multithreading/Main.java
+++ multithreading/Main.java
@@ -3,5 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int iterations  = 3;
+
+		MyThread thread = new MyThread();
+
+		thread.start();
 	}
 }
--- multithreading/MyThread.java
+++ multithreading/MyThread.java
@@ -1,13 +1,14 @@
 
 public class MyThread extends Thread{
 
+	// implement the run method 
 	@Override
 	public void run(){
 		int iterator = 5;
 
 		for (int i = 0; i < iterator ; i++ ) {
 			System.out.println("Output from secondary thread.");
-			sleep(5000);
+			sleep(2000);
 		}
 	}
 }
