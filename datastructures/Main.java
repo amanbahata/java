683afc88ff3a90c7aa131cd4f164e37e911a3617
Aman Bahata <bahataaman@gmail.com>
2017-07-20 11:00:16 +0100

    Adds datastructures folder and creates a Main class with basic Arrays

---
+++ datastructures/Main.java
@@ -0,0 +1,24 @@
+/**
+* Exsercise with javas built in datastructures
+* Arrays and java inbuilt lists
+*/
+
+public class Main{
+	public static void main(String[] args) {
+		
+		// Arrays
+		int[] myIntArray = new int[10];
+
+		myIntArray[5] = 50;
+		myIntArray[0] = 70;
+		myIntArray[9] = 100;
+
+		double[] myDoubleArray = new double[5];
+
+		myDoubleArray[3] = 0.5;
+		myDoubleArray[0] = 1.0;
+
+		System.out.println("Element at index 5 of myIntArray is: " + myIntArray[5]);
+		System.out.println("Element at index 3 of myDoubleArray is: " + myDoubleArray[3]);
+	}
+}
