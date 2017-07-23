2e550bf197c4d767ee64379227597781c88cccd6
Aman Bahata <bahataaman@gmail.com>
2017-07-23 19:17:23 +0100

    Adds class Main to create and a run a Car object

--- oop/Main.java
+++ oop/Main.java
@@ -2,7 +2,12 @@ public class Main{
 	public static void main(String[] args) {
 
 
-		Vehicle ford = new Car("Ford", 5, 300);
-		
+		Car ford = new Car("Ford", 5, 300);
+
+		System.out.println("The vehicles name is: " + ford.getName());
+		System.out.println("The vehicles number of doors: " + ford.getDoors());
+		System.out.println("The vehicles engine capacity is : " + ford.getEngineCapacity() + " cc.");
+
+
 	}
 }
