ff40c4dce208901425bd087c9fe6505daaf25906
Aman Bahata <bahataaman@gmail.com>
2017-09-07 14:04:24 +0100

    Creates a circle and adds as child node to root

--- javaFX/src/sample/Main.java
+++ javaFX/src/sample/Main.java
@@ -4,16 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        // add a leaf node
+        // add a leaf node Circle
+        Circle cir = new Circle(200, 200, 100);
+        // Add colour to the circle
+        cir.setFill(Color.CORAL);
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        StackPane root = new StackPane();
+
+        // Attach the circle to root so it can be displayed
+        root.getChildren().add(cir);
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
