b399e29b49792b18b4e72d95e5f8353d957af0d7
Aman Bahata <bahataaman@gmail.com>
2017-09-07 15:19:47 +0100

    Adds a stop traffic sign

--- javaFX/circle/src/sample/Main.java
+++ javaFX/circle/src/sample/Main.java
@@ -1,41 +1,35 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-        // add a leaf node Circle
-        Circle cir = new Circle(200, 200, 100);
-        // Add a leaf node Rectangle
-        Rectangle rec = new Rectangle(200, 200, 140, 40);
-
-        // Add colour to the circle
-        cir.setFill(Color.RED);
-        // Add color to the rectangle
-        rec.setFill(Color.WHITE);
-
         StackPane root = new StackPane();
 
-        // Attach the circle to root so it can be displayed
+        // Add a leaf node
+        Circle cir = new Circle(200, 200, 100);
+        Rectangle rec = new Rectangle(200, 200, 150, 40);
+
+        cir.setFill(Color.RED);
+        rec.setFill(Color.WHITE);
+
+
         root.getChildren().add(cir);
         root.getChildren().add(rec);
 
-        primaryStage.setTitle("Scene Graph Example.");
-        primaryStage.setScene(new Scene(root, 400, 375));
+
+        primaryStage.setTitle("Scene Graph Example");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
