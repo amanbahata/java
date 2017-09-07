cd336199e594563f20f2b2c4c40662f74fa16bdd
Aman Bahata <bahataaman@gmail.com>
2017-09-07 16:12:30 +0100

    Adds a simple application with a single button

---
+++ javaFX/HelloWorld/src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        StackPane root = new StackPane();
+
+        Button button = new Button("Press here !");
+        button.setOnAction(event -> System.out.println("Button pressed"));
+
+        root.getChildren().add(button);
+
+        primaryStage.setTitle("Buttons Test");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
