2e3a043343aadfe4cdbdb64e3587c76b38e8023f
Aman Bahata <bahataaman@gmail.com>
2017-09-07 14:32:21 +0100

    changes the color of the circle to red and adds a white rectangle in the center in order to form a STOP traffic sign

--- javaFX/src/sample/Main.java
+++ javaFX/src/sample/Main.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -19,16 +21,22 @@ public class Main extends Application {
 
         // add a leaf node Circle
         Circle cir = new Circle(200, 200, 100);
+        // Add a leaf node Rectangle
+        Rectangle rec = new Rectangle(200, 200, 140, 40);
+
         // Add colour to the circle
-        cir.setFill(Color.CORAL);
+        cir.setFill(Color.RED);
+        // Add color to the rectangle
+        rec.setFill(Color.WHITE);
 
         StackPane root = new StackPane();
 
         // Attach the circle to root so it can be displayed
         root.getChildren().add(cir);
+        root.getChildren().add(rec);
 
-        primaryStage.setTitle("Circle drawing tutorial");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Scene Graph Example.");
+        primaryStage.setScene(new Scene(root, 400, 375));
         primaryStage.show();
     }
 
