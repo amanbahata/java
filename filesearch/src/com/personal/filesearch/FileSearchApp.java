1f23fcbd0197cc3a5e1598a070bbc90d85509867
Aman Bahata <bahataaman@gmail.com>
2017-09-08 20:13:48 +0100

    adds searchFile, addFileTOZip, walkDirectory methods to the class

--- filesearch/src/com/personal/filesearch/FileSearchApp.java
+++ filesearch/src/com/personal/filesearch/FileSearchApp.java
@@ -1,5 +1,7 @@
 package com.personal.filesearch;
 
+import java.io.File;
+
 /**
  * Created by aman1 on 08/09/2017.
  */
@@ -13,12 +15,44 @@ public class FileSearchApp {
     private String regex;
     private String zipFileName;
 
-    
+
     public static void main(String[] args){
 
-        FileSearchApp fileSearchApp = new FileSearchApp();
+        FileSearchApp app = new FileSearchApp();
+
+        switch (Math.min(args.length, 3)){
+            case 0 : System.out.println("USAGE: FileSearchApp path [regex] [zipfile]");
+                    return;
+            case 3 : app.setZipFileName(args[2]);
+            case 2 : app.setPath(args[1]);
+            case 1 : app.setRegex(args[0]);
+
+        }
+
+        try {
+            app.walkDirectory(app.getPath());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
+    }
+
+
+    public void walkDirectory(String path){
+        System.out.println("Directory: " + path );
+        searchFile(null);
+        addFileToZip(null);
+    }
+
+
+    public void searchFile(File file){
+        System.out.println("searchFile: " + file);
+
+    }
+
+    public void addFileToZip(File file){
+        System.out.println("addFileToZip: " + file);
     }
 
     /**
