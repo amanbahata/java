527d91aef2e7bb9826070a082e01ec43066c2e42
Aman Bahata <bahataaman@gmail.com>
2017-09-08 19:47:30 +0100

    Creates the FileSearchApp class and adds instance variables together their accessor methods

--- filesearch/src/com/personal/filesearch/FileSearch.java
+++
@@ -1,7 +0,0 @@
-package com.personal.filesearch;
-
-/**
- * Created by aman1 on 08/09/2017.
- */
-public class FileSearch {
-}
---
+++ filesearch/src/com/personal/filesearch/FileSearchApp.java
@@ -0,0 +1,24 @@
+package com.personal.filesearch;
+
+/**
+ * Created by aman1 on 08/09/2017.
+ */
+public class FileSearchApp {
+
+    private String path;
+    private String regex;
+    private String zipFileName;
+
+    public static void main(String[] args){
+
+
+    }
+
+    public String getPath(){
+        return path;
+    }
+
+    public String getRegex(){
+        return regex;
+    }
+}
